9d2f2c8462683c3a0004f6f02c73d439454c1810
prog106 <prog106@gmail.com>
2021-06-10 15:16:27 +0900

    'add'

---
+++ src/service/firebase.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
